10757b06897388da3d222d96078f2fb49ca646ad
Govindkrg <govindkr046@gmail.com>
2025-05-02 20:33:18 +0530

    initial commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,19 +12,19 @@
                         @csrf
                         <div class="mb-3">
                             <label for="username" class="form-label">Username</label>
-                            <input type="text" class="form-control" id="username" name="name" required>
+                            <input type="text" class="form-control" id="username" placeholder="Enter Name" name="name" required>
                         </div>
                         <div class="mb-3">
                             <label for="email" class="form-label">Email</label>
-                            <input type="email" class="form-control" id="email" name="email" required>
+                            <input type="email" class="form-control" id="email" placeholder="Enter Email" name="email" required>
                         </div>
                         <div class="mb-3">
                             <label for="password" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="password" name="password" required>
+                            <input type="password" class="form-control" id="password" placeholder="Enter Password" name="password" required>
                         </div>
                         <div class="mb-3">
                             <label for="password_confirmation" class="form-label">Confirm Password</label>
-                            <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
+                            <input type="password" class="form-control" id="password_confirmation" placeholder="Enter Confirm Password"  name="password_confirmation" required>
                         </div>
                         <button type="submit" class="btn btn-primary">Register</button>
                     </form>
